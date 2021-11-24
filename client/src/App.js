fd78a8dcbe34363c3908b0dfa5a0b2d6def0885f
jinimbrancalhao <jinjinim717@gmail.comgit config --global user.email jinjinim717@gmail.com>
2021-11-23 19:47:01 -0500

    react router and browserrouter set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import './App.css'
+import { Route, Switch } from 'react-router-dom'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
     </div>
   )
 }
